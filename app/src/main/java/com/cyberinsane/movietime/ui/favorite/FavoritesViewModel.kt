7b0c6677d0bb77dde499948aa42a8cd3f2dbe0b7
Rao, Vineet (US - Mumbai) <vinrao@deloitte.com>
2020-03-21 15:45:36 +0530

    Base setup with bottom nav and navigation controller

---
+++ app/src/main/java/com/cyberinsane/movietime/ui/favorite/FavoritesViewModel.kt
@@ -0,0 +1,13 @@
+package com.cyberinsane.movietime.ui.favorite
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class FavoritesViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is favorite Fragment"
+    }
+    val text: LiveData<String> = _text
+}
